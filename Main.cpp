d1371d089c4967ae14c1ca4b5d287d17989f20f8
Munzir Zafar <mzafar7@gatech.edu>
2018-04-26 14:15:51 -0400

    added dq filtering

--- Main.cpp
+++ Main.cpp
@@ -201,7 +201,7 @@ int main(int argc, char* argv[])
   // create and initialize the world
   //Eigen::Vector3d gravity(0.0,  -9.81, 0.0);
   //world->setGravity(gravity);
-  world->setTimeStep(1.0/5000);
+  world->setTimeStep(1.0/1000);
 
   // create a window and link it to the world
   MyWindow window(new Controller(robot, robot->getBodyNode("lGripper"), robot->getBodyNode("rGripper") ) );
